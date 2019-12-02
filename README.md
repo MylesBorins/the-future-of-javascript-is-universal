4f9653cb474f6176f305bc69cf6c48bc5bb483e9
Myles Borins <mylesborins@google.com>
2019-12-02 01:11:28 -0500

    make it so

---
+++ README.md
@@ -0,0 +1,30 @@
+# Extra Special Modules
+> A [Bespoke.js](http://markdalgleish.com/projects/bespoke.js) presentation, built with [generator-bespoke](https://github.com/markdalgleish/generator-bespoke)
+
+## Abstract
+
+The Node.js modules team has finally got there! With Node.js 12.x we shipped a new implementation to our EcmaScript Modules (ESM) implementation with plans to unflag by LTS. This talk will cover the history of ESM in Node.js, the iterations of the implementation, and catch you up as to how things currently work.
+
+## A bit more info
+
+This talk will cover content for Node.js application developers, Front End application developers, module authors, and anyone who is interested in how platform decisions get made.
+ diverge; This is their story.
+
+### Watch The Talk
+
+[![Earliest iteration at Node.js Interactive 2017](https://img.youtube.com/vi/W5CXzo4TZVU/0.jpg)](https://www.youtube.com/watch?v=W5CXzo4TZVU)
+
+[![Now out of date iteration from JSConf EU 2018](https://img.youtube.com/vi/35ZMoH8T-gc/0.jpg)](https://www.youtube.com/watch?v=35ZMoH8T-gc)
+
+## View slides locally
+
+First, ensure you have the following installed:
+
+1. [Node.js](http://nodejs.org)
+
+Then, install dependencies and run the preview server:
+
+```bash
+$ npm install
+$ npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+'use strict';
+const path = require('path');
+
+const {
+  series,
+  parallel,
+  watch,
+  src,
+  dest
+} = require('gulp');
+
+const autoprefixer = require('gulp-autoprefixer');
+const browserify = require('gulp-bro');
+const connect = require('gulp-connect');
+const csso = require('gulp-csso');
+const del = require('del');
+const jade = require('gulp-pug');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const stylus = require('gulp-stylus');
+const through = require('through');
+const uglify = require('gulp-uglify');
+
+const isDist = process.argv.indexOf('serve') === -1;
+
+async function cleanJS() {
+  await del('dist/build/build.js');
+}
+
+function buildJS() {
+  return src('src/scripts/main.js')
+    .pipe(isDist ? through() : plumber())
+    .pipe(browserify({ debug: !isDist }))
+    .pipe(isDist ? uglify() : through())
+    .pipe(rename('build.js'))
+    .pipe(dest('dist/build'))
+    .pipe(connect.reload());
+}
+
+exports.js = series(cleanJS, buildJS);
+
+async function cleanHTML() {
+  await del('dist/index.html');
+}
+
+function buildHTML() {
+  return src('src/index.jade')
+    .pipe(isDist ? through() : plumber())
+    .pipe(jade({ pretty: true }))
+    .pipe(rename('index.html'))
+    .pipe(dest('dist'))
+    .pipe(connect.reload());
+}
+
+exports.html = series(cleanHTML, buildHTML);
+
+async function cleanCSS() {
+  await del('dist/build/build.css');
+}
+
+function buildCSS() {
+  return src('src/styles/main.styl')
+    .pipe(isDist ? through() : plumber())
+    .pipe(stylus({
+      // Allow CSS to be imported from node_modules and bower_components
+      'include css': true,
+      'paths': ['./node_modules', './bower_components']
+    }))
+    .pipe(autoprefixer('last 2 versions', { map: false }))
+    .pipe(isDist ? csso() : through())
+    .pipe(rename('build.css'))
+    .pipe(dest('dist/build'))
+    .pipe(connect.reload());
+}
+
+exports.css = series(cleanCSS, buildCSS);
+
+async function cleanImages() {
+  await del('dist/images');
+}
+
+function copyImages() {
+  return src('src/images/**/*')
+    .pipe(dest('dist/images'))
+    .pipe(connect.reload());
+}
+
+exports.images = series(cleanImages, copyImages)
+
+async function cleanTask () {
+  await del('dist');
+}
+exports.clean = cleanTask;
+
+function connectTask(done) {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+  done();
+}
+
+function watchTask(done) {
+  watch('src/**/*.jade', exports.html);
+  watch('src/styles/**/*.styl', exports.css);
+  watch('src/images/**/*', exports.images);
+  watch([
+    'src/scripts/**/*.js',
+    'bespoke-theme-*/dist/*.js' // Allow themes to be developed in parallel
+  ], exports.js);
+  done();
+}
+
+exports.build = parallel(exports.js, exports.html, exports.css, exports.images);
+
+exports.connect = series(exports.build, connectTask)
+
+exports.serve = parallel(exports.connect, watchTask);
+
+exports.default = exports.build;
+
