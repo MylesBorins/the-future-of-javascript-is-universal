c2d8827fb6e12174cc00a2dd072df19f2ef0022a
Myles Borins <mylesborins@google.com>
2019-12-11 03:27:47 -0500

    coming along

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,18 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Require Node modules in the browser thanks to Browserify: http://browserify.org
-
-var bespoke = require('bespoke'),
-  nebula = require('bespoke-theme-nebula'),
-  keys = require('bespoke-keys'),
-  touch = require('bespoke-touch'),
-  bullets = require('bespoke-bullets'),
-  backdrop = require('bespoke-backdrop'),
-  scale = require('bespoke-scale'),
-  hash = require('bespoke-hash'),
-  // progress = require('bespoke-progress'),
-  forms = require('bespoke-forms');
+const bespoke = require('bespoke');
+const nebula = require('bespoke-theme-nebula');
+const keys = require('bespoke-keys');
+const touch = require('bespoke-touch');
+const bullets = require('bespoke-bullets');
+const backdrop = require('bespoke-backdrop');
+const scale = require('bespoke-scale');
+const hash = require('bespoke-hash');
+// const progress = require('bespoke-progress');
+const forms = require('bespoke-forms');
 
 // Bespoke.js
 bespoke.from('article', [
